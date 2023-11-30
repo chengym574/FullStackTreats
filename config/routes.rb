e0b194a43a2a7ab672d85d5e6bb145267a3d84a2
Louis Cheng <ycheng33@academic.rrc.ca>
2023-11-30 13:16:09 -0600

    Adding signup login logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+  }
+  
   root to: "home#index"
 end
