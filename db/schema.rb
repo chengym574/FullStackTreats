2933e8f336f8a7547e6009972be12754f0b3525c
Louis Cheng <ycheng33@academic.rrc.ca>
2023-12-06 17:15:55 -0600

    Changing user type attribute to role

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_184405) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "type"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
