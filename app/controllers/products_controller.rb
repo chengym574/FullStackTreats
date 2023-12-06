7b23ee193a368654c5d3f717c92fa61f9c2d7d3b
Louis Cheng <ycheng33@academic.rrc.ca>
2023-12-06 17:02:58 -0600

    Search functionality in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,19 @@ class ProductsController < ApplicationController
     def show
       @product = Product.find(params[:id])
     end
+
+    def search
+      @products = Product.includes(:category)
+
+      if params[:search].present?
+        @products = Product.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+      end
+      if params[:category].present?
+        @products = @products.where(category_id: params[:category])
+      end
+      
+      @products = @products.all.page(params[:page]).per(10)
+      @categories = Category.all 
+      render 'home/index'
+    end
 end
