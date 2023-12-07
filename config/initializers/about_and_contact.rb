376c77298a8180287c335f811b93b9c9a7b70b36
Louis Cheng <ycheng33@academic.rrc.ca>
2023-12-07 09:50:28 -0600

    Environment variables for about and contact

---
+++ config/initializers/about_and_contact.rb
@@ -0,0 +1,2 @@
+ENV['ABOUT_CONTENT'] ||= 'Full Stack Treats is a local, family-owned bakery specializing in crafting homemade pastries, cookies, and muffins. With a team of two, Full Stack Treats has been satisfying the sweet tooth of the community for just over 2 years.'
+ENV['CONTACT_CONTENT'] ||= 'For any inquiries you may have, please reach our to fullstacktreats@gmail.com'
