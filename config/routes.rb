27a9267beaba722e4993e501faecb475b8742bb8
Louis Cheng <ycheng33@academic.rrc.ca>
2023-12-07 09:51:23 -0600

    Routes for about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  # get 'home/index'
+  get 'about', to: 'pages#show_about', as: :pages_about
+  get 'contact', to: 'pages#show_contact', as: :pages_contact
+
+  get 'about/edit', to: 'pages#edit_about', as: :edit_about
+  patch 'about/update', to: 'pages#update_about', as: :update_about
+
+  get 'contact/edit', to: 'pages#edit_contact', as: :edit_contact
+  patch 'contact/update', to: 'pages#update_contact', as: :update_contact
 
   devise_for :users, controllers: {
   sessions: 'users/sessions',
